67c6d25bd529edd25753c471d01b2d7fce962c95
mil0xeth <mil0x.eth@gmail.com>
2022-11-17 18:27:09 +0100

    fix healtcheck and usdc tests

--- tests/Optimism/test_delta_credit.py
+++ tests/Optimism/test_delta_credit.py
@@ -3,7 +3,7 @@ from brownie import Contract, ZERO_ADDRESS
 import pytest
 
 def test_limited_delta_credit_profit(
-    chain, accounts, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX, gov, token_LP_whale, stargate_token_pool
+    chain, accounts, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX, gov, token_LP_whale, stargate_token_pool, token_lp
 ):
     # 1- Deposit to the vault
     token.approve(vault.address, amount, {"from": user})
@@ -18,13 +18,12 @@ def test_limited_delta_credit_profit(
 
     # 3- Simulate profit via want airdrop of S*token
     chain.sleep(1)
-    whale = Contract("0xB0D502E938ed5f4df2E681fE6E419ff29631d62b")
-    stargate_token_pool.transfer(strategy.address, amount, {"from": whale})
+    stargate_token_pool.transfer(strategy.address, amount, {"from": token_LP_whale})
 
     # 4- Ensure that there is no sufficient deltaCredit
     liquidityPool = Contract(strategy.liquidityPool())
     router = Contract(liquidityPool.router())
-    router.instantRedeemLocal(liquidityPool.poolId(), liquidityPool.deltaCredit(), strategist, {"from":token_LP_whale})
+    router.instantRedeemLocal(liquidityPool.poolId(), min(liquidityPool.deltaCredit(), token_lp.balanceOf(token_LP_whale)), strategist, {"from":token_LP_whale})
 
     # 5- Call another harvest and see if profit is repported correctly
     assert liquidityPool.deltaCredit() < amount
